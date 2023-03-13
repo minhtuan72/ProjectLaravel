08b200a4ebad67192bff4d8cf646321c7ac18a73
minhtuan72 <minhtuan070220@gmail.com>
2023-03-13 16:35:13 +0700

    commit project demoLrv10 v1

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DemoLrv10</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <!-- <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/> -->
+    
+    <style type="text/css">    
+        @import url(https://fonts.googleapis.com/css?family=Raleway:300,400,600);
+  
+        body{
+           
+            /* background: url(../../image/banner1.jpg)no-repeat; */
+            background: url({{ Vite::asset('resources/image/banner1.jpg') }})no-repeat;
+            background-attachment: fixed;
+                background-position: center;
+                background-size: cover;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+            padding:0;
+            margin: 0;
+            font-size: .9rem;
+            font-weight: 400;
+            line-height: 1.6;
+            color: #212529;
+            text-align: left;
+            
+            /* background-color: #000000; */
+           
+            /* background-image: url("F:\WorkSpace\demoLrv10\resources\image\anh1.jpg"); */
+        }
+        .navbar-laravel
+        {
+            background-color: rgba(247, 247, 247, 1);
+            box-shadow: 0 2px 4px rgba(0,0,0,.04);
+        }
+        .navbar-brand , .nav-link, .my-form, .login-form
+        {
+            
+            font-family: Raleway, sans-serif;
+        }
+        .card-body
+        {
+            background: url({{ Vite::asset('resources/image/anh1.jpg') }})no-repeat;
+            background-attachment: fixed;
+                 /* background-position: center; */
+                background-size: cover;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover; 
+        }
+        .my-form
+        {
+            padding-top: 1.5rem;
+            padding-bottom: 1.5rem;
+        }
+        .my-form .row
+        {
+            margin-left: 0;
+            margin-right: 0;
+        }
+        .login-form
+        {
+            
+            padding-top: 1.5rem;
+            padding-bottom: 1.5rem;
+        }
+        .login-form .row
+        {
+            
+            margin-left: 0;
+            margin-right: 0;
+        }
+        /* .nav-link
+        {
+            color: #B22222;
+        } */
+    </style>
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="#">Laravel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+   
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}"><font color="#FF0000">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}"><font color="#FF0000">Register</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"><font color="#FF0000">Logout</a>
+                    </li>
+                @endguest
+            </ul>
+  
+        </div>
+    </div>
+</nav>
+  
+@yield('content')
+     
+</body>
+</html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="F:\WorkSpace\demoLrv10\resources\css\style.css" rel="stylesheet" type="text/css" media="all"/>
+    <title>Hello</title>
+</head>
+<body>
+    Hello
+</body>
+</html>
